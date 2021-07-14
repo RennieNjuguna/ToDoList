b66c27f5e986498c0c2ed6b04ae5059f7106620b
RennieNjuguna <rennynjuguna@gmail.com>
2021-07-14 13:26:19 +0300

    adding files

---
+++ components/List.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { Colours } from './Colours';
+import { Icon } from 'react-native-elements'
+
+
+export function List(props) {
+
+  const Renderer = ({item}) => (
+    <View style={ (item.status === true) ? ListStyle.listItemDone : ListStyle.listItem}>
+      <Text>{item.name}</Text>
+      <View style={ListStyle.buttons}>
+        <TouchableOpacity onPress={ () => props.doneHandler(item.id) }>
+          <Icon name="check" color={ Colours.secondary }/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={ () => props.deleteHandler(item.id) }>
+          <Icon name="delete" color={ Colours.tertiary }/>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+
+  return(
+    <View>
+      <FlatList 
+      data={ props.listItems } 
+      renderItem={ Renderer} 
+      keyExtractor={ item => item.id.toString() } 
+    />
+    </View>
+  )
+}
+
+const ListStyle = StyleSheet.create( {
+  listItem: {
+    backgroundColor: Colours.light,
+    marginVertical: 4,
+    fontSize: 18,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  listItemDone: {
+    backgroundColor: Colours.tertiary,
+    marginVertical: 4,
+    fontSize: 18,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  buttons: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+})
